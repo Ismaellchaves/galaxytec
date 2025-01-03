300f0453e8f5b17f23402a262d7dfc0a39da7393
IsmaellChaves <ismaellmael139@gmail.com>
2025-01-02 23:41:38 -0300

    Initial commit

---
+++ lojaTec-landingPage-main/assets/menuMobile.js
@@ -0,0 +1,15 @@
+const menuMobile = document.querySelector(".botoes_de_acesso ul");
+const botaoMobile = document.querySelector(".btnMobile span");
+
+function abrirEFecharMenu() {
+    menuMobile.classList.toggle("ativo")
+    botaoMobile.classList.toggle("ativo")
+
+    if(botaoMobile.classList.contains("ativo")){
+        botaoMobile.innerHTML = "close"
+    } else{
+        botaoMobile.innerHTML = "menu"
+    }
+}
+
+botaoMobile.addEventListener("click", abrirEFecharMenu);
